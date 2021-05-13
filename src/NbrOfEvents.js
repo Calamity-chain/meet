7d0a073086cd3bf5807f17469603a3fc64f6ddbc
Calamity-chain <78163718+Calamity-chain@users.noreply.github.com>
2021-05-13 19:24:12 +0200

    Created Error Alert using OPP

--- src/NbrOfEvents.js
+++ src/NbrOfEvents.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { ErrorAlert } from './Alert';
 
 class NumberOfEvents extends Component {
   state = {
@@ -7,15 +8,29 @@ class NumberOfEvents extends Component {
 
   handleEventInputChanged = (event) => {
     const eventValue = event.target.value;
-    this.setState({
-      eventValue,
-    });
+    if (eventValue > 1) {
+      return this.setState({
+        eventValue:'',
+        errorText:'Select number between 1 and 32',
+      });
+    }else if (eventValue > 32) {
+      return this.setState({
+        eventValue:'',
+        errorText:'Select number between 1 and 32',
+      });
+    }else{
+      this.setState({
+        eventValue,
+        errorText:'',
+      });
     this.props.updateEvents(null, eventValue);
+    } 
   };
 
   render() {
     return (
       <div className='event-number'>
+        <ErrorAlert text={this.state.errorText}/>
         <label htmlFor='numberOfEvent'>Number of Events:</label>
         <input
           type='number'
